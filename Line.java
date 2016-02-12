0d80a406e705cb5670b27fc4f9a43181571e030d
daltonwu <dalton.z.wu@gmail.com>
2016-02-12 11:47:55 -0500

    Update

--- Line.java
+++ Line.java
@@ -2,10 +2,35 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class Line {
+	private static final int XRES = 500;
+	private static final int YRES = 500;
+	private static int[][][] p = new int[XRES][YRES][3];
+	
+	private static void plotLine(int x0, int y0, int x1, int y1) {
+		final double A = m; // Δy
+		final double B = 1; // -Δx
+		final double d = 2*A + B;
+	}
+	
+	private static void plot(int x, int y, int[] rgb) {
+		System.arraycopy(rgb, 0, p[x][y], 0, 3);
+	}
 	public static void main(String[] args) {
 		Random rand = new Random();
+		int[][][] p = new int[XRES][YRES][3];
+		int num = 5; // how many lines to draw
+		double m; // random slope in interval [-5, 5]
 		
-		int[][][] p = new int[500][500][3];
+		while(num > 0) {
+			m = 10*rand.nextDouble() - 5;
+			plotLine(x, y, m);
+			
+			final double A = m; // Δy
+			final double B = 1; // -Δx
+			final double D = 2A + B;
+			while(x <= 
+			num--;
+		}
 		
 		PrintWriter w = null;
 		try {
@@ -16,7 +41,7 @@ public class Line {
 		}
 		
 		w.println("P3");
-		w.println("500 500 255");
+		w.println(XRES + " " + YRES + " 255");
 		
 		for(int i = 0; i < 500; i++) {
 			for(int j = 0; j < 500; j++) {
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+	public static void main(String[] args) {
+		int foo = 123;
+		System.out.println(2 foo);
+	}
+}
